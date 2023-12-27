89d127855a4070bb369bc06e31af999f1065e22a
huang-ju <huangjugood@gmail.com>
2023-12-27 10:06:26 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-table
+      :loading="isLoading"
+      :columns="columns"
+      :data-source="dataList"
+      :pagination="pagination"
+    >
+    </a-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EosApi from "eosjs-api";
 
+const columns = [
+  {
+    title: "序号",
+    key: "index",
+    dataIndex: "index",
+  },
+  { title: "账号", dataIndex: "username", key: "username" },
+  { title: "RAMS数量", dataIndex: "mintcount", key: "mintcount" },
+];
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      isLoading: false,
+      columns: columns,
+      dataList: [],
+      pagination: {
+        defaultPageSize: 100,
+      },
+    };
+  },
+  mounted() {
+    this.isLoading = true;
+    const eos = EosApi({
+      httpEndpoint: "https://eos.greymass.com",
+      verbose: false,
+      fetchConfiguration: {},
+    });
+    eos
+      .getTableRows({
+        json: true,
+        code: "rams.eos",
+        scope: "rams.eos",
+        table: "users",
+        lower_bound: null,
+        upper_bound: null,
+        index_position: 1,
+        key_type: "",
+        limit: "10000",
+        reverse: false,
+        show_payer: true,
+      })
+      .then((res) => {
+        this.dataList = res.rows.map((v) => {
+          return {
+            username: v.payer,
+            mintcount: v.data.mintcount,
+          };
+        });
+        this.dataList = this.dataList
+          .sort((a, b) => b.mintcount - a.mintcount)
+          .map((v, index) => {
+            return {
+              ...v,
+              index: index + 1,
+            };
+          });
+      })
+      .catch((err) => {
+        console.log("err", err);
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+  },
+  methods: {},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-weight: bold;
 }
 </style>
